1e4955c6223b2d55f33cfd82c13423ff68ec9f6a
Niel McLaren <d@nielmclaren.com>
2025-12-04 12:03:28 -0800

    Day two part two.

--- 2025/day2/main.py
+++ 2025/day2/main.py
@@ -4,24 +4,22 @@ import math
 import re
 
 # Guesses: 27180728081 (too high), 37556907 (too low), 19219513675 (too high), 19219508902 (just right!)
+# Guesses:
 
 
 def solve(num):
     num_str = str(num)
     length = len(num_str)
 
-    if length % 2 != 0:
-        return False
-
-    i = int(length / 2)
-    if i > 0:
-        pattern = num_str[0:i]
-        count = math.floor(length / i)
-        regex = f"^({pattern}){{{count}}}$"
-        result = re.search(regex, num_str)
-        if result:
-            print(num_str, pattern)
-            return True
+    for i in range(1, length):
+        if length % i == 0:
+            pattern = num_str[0:i]
+            count = math.floor(length / i)
+            regex = f"^({pattern}){{{count}}}$"
+            result = re.search(regex, num_str)
+            if result:
+                print(num_str, pattern)
+                return True
 
     return False
 
@@ -52,8 +50,6 @@ def main():
 
 
 def test():
-    #positive_cases = [11, 111, 1111, 1212, 11111, 111111, 121212, 123123]
-    #negative_cases = [12, 121, 1221, 12121, 21212, 11211, 121211, 123121]
     positive_cases = [11, 111, 1111, 1212, 11111, 111111, 121212, 123123]
     negative_cases = [12, 121, 1221, 12121, 21212, 11211, 121211, 123121]
 
