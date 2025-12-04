68c34704a8df6ce679232f3431f5b25b1011f424
Niel McLaren <d@nielmclaren.com>
2025-12-04 13:16:15 -0800

    Switch to globals for grid width and height.

--- 2025/day4/main.py
+++ 2025/day4/main.py
@@ -9,19 +9,23 @@ import re
 THRESHOLD = 4
 
 
-def grid_value(grid, grid_width, x, y):
+grid_width = -1
+grid_height = -1
+
+
+def grid_value(grid, x, y):
     return grid[y * grid_width + x]
 
 
-def is_in_grid(grid_width, grid_height, x, y):
+def is_in_grid(x, y):
     return x >= 0 and x < grid_width and y >= 0 and y < grid_height
 
 
-def is_accessible(grid, grid_width, grid_height, cx, cy):
+def is_accessible(grid, cx, cy):
     count = 0
     for x in range(cx - 1, cx + 2):
         for y in range(cy - 1, cy + 2):
-            if (cx != x or cy != y) and is_in_grid(grid_width, grid_height, x, y) and grid_value(grid, grid_width, x, y):
+            if (cx != x or cy != y) and is_in_grid(x, y) and grid_value(grid, x, y):
                 count += 1
                 if count >= THRESHOLD:
                     return False
@@ -29,19 +33,21 @@ def is_accessible(grid, grid_width, grid_height, cx, cy):
     return True
 
 
-def count_accessible_cells(grid, grid_width, grid_height):
+def count_accessible_cells(grid):
     result = 0
     for cx in range(grid_width):
         for cy in range(grid_height):
-            if grid_value(grid, grid_width, cx, cy) and is_accessible(grid, grid_width, grid_height, cx, cy):
-                #print(f"{cx},{cy}", grid_value(grid, grid_width, cx, cy))
+            if grid_value(grid, cx, cy) and is_accessible(grid, cx, cy):
+                #print(f"{cx},{cy}", grid_value(grid, cx, cy))
                 result += 1
     return result
 
 
 def solve(filename):
+    global grid_width
+    global grid_height
+
     grid = []
-    grid_width = -1
 
     with open(filename) as file:
         for line in file:
@@ -50,10 +56,10 @@ def solve(filename):
             if grid_width < 0:
                 grid_width = len(grid)
 
-    grid_height = int(len(grid) / grid_width)
+        grid_height = int(len(grid) / grid_width)
 
-    result = count_accessible_cells(grid, grid_width, grid_height)
-    print(result)
+        result = count_accessible_cells(grid)
+        print(result)
 
 
 solve("input.txt")
