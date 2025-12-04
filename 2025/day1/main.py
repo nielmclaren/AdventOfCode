7d60be3f863007eec195961e01fcfc75f0e92a57
Niel McLaren <d@nielmclaren.com>
2025-12-04 09:02:28 -0800

    Day one part two.

--- 2025/day1/main.py
+++ 2025/day1/main.py
@@ -3,7 +3,7 @@
 import math
 
 # Guesses: seven
-# Guesses: 6380 (too high)
+# Guesses: 6380 (too high), 5793 (too low), 5887
 
 print("ohai")
 
@@ -12,22 +12,33 @@ prev_pos = 50
 curr_zeros = 0
 total_zeros = 0
 
-with open("test.txt", "r") as file:
+with open("input.txt", "r") as file:
     for line in file:
         sign = 1 if line[0] == 'R' else -1
         dist = int(line[1:])
 
         pos += sign * dist
 
-        if pos >= 100:
-            curr_zeros += math.floor(pos / 100)
-        if pos <= 0:
-            curr_zeros += math.floor(abs(pos) / 100)
+        if pos == 0:
+            curr_zeros += 1
 
-        pos = pos % 100
+        elif pos > 0:
+            while pos >= 100:
+                pos -= 100
+                curr_zeros += 1
 
-        while pos < 0:
-            pos += 100
+        elif pos < 0:
+            # Subtract one zero if starting from zero.
+            if prev_pos == 0 and pos < 0:
+                curr_zeros -= 1
+
+            while pos < 0:
+                pos += 100
+                curr_zeros +=1
+
+            # Add one zero if ending at zero.
+            if pos == 0:
+                curr_zeros += 1
 
         print(f"{line.rstrip()}\t{prev_pos}\t{sign * dist}\t{pos}\t({curr_zeros})")
         total_zeros += curr_zeros
