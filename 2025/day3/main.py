0463699c3dcf10eba51082bd4eae3212423b9a03
Niel McLaren <d@nielmclaren.com>
2025-12-04 12:18:23 -0800

    Day three part two.

--- 2025/day3/main.py
+++ 2025/day3/main.py
@@ -17,24 +17,29 @@ def get_highest_digit_index(bank, start, end):
     return highest_index
 
 
-def calc_max_joltage(bank):
-    tens_digit_index = get_highest_digit_index(bank, 0, len(bank) - 1)
-    tens_digit = bank[tens_digit_index]
-    ones_digit = bank[get_highest_digit_index(bank, tens_digit_index + 1, len(bank))]
-    result = int(f"{tens_digit}{ones_digit}")
+def calc_max_joltage(bank, num_batts):
+    accum = ""
+    digit_index = -1
+    for i in range(num_batts):
+        digit_index = get_highest_digit_index(bank, digit_index + 1, len(bank) - num_batts + i + 1)
+        accum += bank[digit_index]
+
+    result = int(accum)
     print(bank, result)
     return result
 
 
 
-def solve(filename):
+def solve(filename, num_batts):
     result = 0
     with open(filename) as file:
         for line in file:
-            result += calc_max_joltage(line.rstrip())
+            result += calc_max_joltage(line.rstrip(), num_batts)
 
     print(result)
 
 
-solve("input.txt")
-#solve("test.txt")
+#solve("input.txt", 3)
+solve("input.txt", 12)
+#solve("test.txt", 2)
+#solve("test.txt", 12)
