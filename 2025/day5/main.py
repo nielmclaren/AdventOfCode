3301bb769433357d727d956dea0bb4212ec1a86e
Niel McLaren <d@nielmclaren.com>
2025-12-04 21:29:43 -0800

    Naive implementation.

--- 2025/day5/main.py
+++ 2025/day5/main.py
@@ -13,33 +13,28 @@ def read_input(filename):
 
     with open(filename) as file:
         is_reading_ingredients = False
-        print("fresh ranges")
         for line in file:
             if not line.rstrip():
-                print("ingredients")
                 is_reading_ingredients = True
             elif is_reading_ingredients:
-                print("ingredient:", line.rstrip())
                 ingredients.append(int(line.rstrip()))
             else:
-                print("range:", line.rstrip())
                 fresh_range = line.split("-")
                 fresh_ranges.append((int(fresh_range[0]), int(fresh_range[1])))
 
     return (fresh_ranges, ingredients)
 
-def solve(fresh_ranges, ingredients):
-    count = 0
-    for ingredient in ingredients:
-        for fresh_range in fresh_ranges:
-            if fresh_range[0] <= ingredient and ingredient <= fresh_range[1]:
-                count += 1
-                break
-    return count
+def solve(fresh_ranges):
+    fresh_map = {}
+    for fresh_range in fresh_ranges:
+        for i in range(fresh_range[0], fresh_range[1] + 1):
+            fresh_map[str(i)] = True
+    return len(fresh_map)
+
 
 def main():
-    (fresh_ranges, ingredients) = read_input("input.txt")
-    result = solve(fresh_ranges, ingredients)
+    (fresh_ranges, _) = read_input("input.txt")
+    result = solve(fresh_ranges)
     print("Result:", result)
 
 
